649ebf8a0236e8e7c35994241e1f6738e38a08b8
Karan2310 <karandeep9619@gmail.com>
2023-07-29 01:46:55 +0530

    Analytics vercel

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
+      <Analytics />
     </>
   );
 }
