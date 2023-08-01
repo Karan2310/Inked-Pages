082f80682699b07085f132977cf09a8aa9b3c431
Karan2310 <karandeep9619@gmail.com>
2023-08-01 19:35:18 +0530

    Token Expiry Increased

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -37,7 +37,7 @@ export const login = async (req, res) => {
     if (!isMatch) return res.status(400).json({ msg: "Invalid Credentials" });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "1d",
+      expiresIn: "7d",
     });
     res
       .status(200)
